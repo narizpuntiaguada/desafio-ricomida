ed5e7843825dba43d39a422c5f992a7be329080e
andrea <arodriguezlopez@gmail.com>
2024-03-05 22:09:16 -0300

    toltips boton enviar

---
+++ assets/js/ricomida.js
@@ -0,0 +1,24 @@
+// link js habilitar tooltips y alerta 
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+$("#enviar_correo").on("click", function () {
+    alert("El correo fue enviado correctamente.");
+});
+
+$("h3").on("dblclick", function () {
+    $(this).css("color", "red")
+});
+
+
+// Jquery para ocultar tarjetas
+$(".card-title").on("click", function () {
+    $(".card-text").toggle("slow");
+});
+$(".card-title-1").on("click", function () {
+    $(".card-text-1").toggle("slow");
+});
+$(".card-title-2").on("click", function () {
+    $(".card-text-2").toggle("slow");
+});
