13bc23396284b6a7aa505fa8ea5d44e951ce581a
andrea <arodriguezlopez@gmail.com>
2024-03-05 23:09:25 -0300

    reseña en js

--- assets/js/ricomida.js
+++ assets/js/ricomida.js
@@ -3,6 +3,8 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
+
+// evento doble click
 $("#enviar_correo").on("click", function () {
     alert("El correo fue enviado correctamente.");
 });
